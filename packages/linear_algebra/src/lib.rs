5bc32c5c78915af46c02f457ed9a9267df1a2936
marcerhans <marcerhans@gmail.com>
2024-03-12 16:52:06 +0100

    Added tests for slice.

--- packages/linear_algebra/src/lib.rs
+++ packages/linear_algebra/src/lib.rs
@@ -568,6 +568,44 @@ pub mod matrix {
             }
         }
 
+        mod test_scalar {
+            use super::*;
+
+            #[test]
+            fn scalar() {
+                let mut matrix = Matrix::from_array([
+                    [1,2,3],
+                    [4,5,6],
+                ]);
+
+                matrix.scalar(2);
+
+                assert!(matrix == Matrix::from_array([
+                    [2,4,6],
+                    [8,10,12],
+                ]))
+            }
+        }
+
+        mod test_slice {
+            use super::*;
+
+            #[test]
+            fn slice() {
+                let matrix = Matrix::from_array([
+                    [1,2,3,4],
+                    [5,6,7,8],
+                    [9,10,11,12],
+                ]);
+
+                let slice = matrix.slice(1..3, 1..3);
+                let check = vec![6,7,10,11];
+                let check_ref = check.iter().collect::<Vec<&i64>>();
+
+                assert!(slice == check_ref);
+            }
+        }
+
         mod test_partial_eq {
             use super::*;
 
