6c56f4bec52de109af683101306b62aa2f727a3c
marcerhans <marcerhans@gmail.com>
2024-03-14 18:12:02 +0100

    ...

--- packages/renderer/src/strategy/mod.rs
+++ packages/renderer/src/strategy/mod.rs
@@ -1,4 +1,3 @@
-// TODO: These should be guarded with a feature flag or similar. Currently only terminal is implemented.
+// TODO: These should be guarded with a feature flag or similar. Currently only terminal is implemented, so it does not really matter.
 pub mod terminal;
 pub use terminal as renderer;
-
