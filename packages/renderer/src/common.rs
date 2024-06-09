50e4c507b143aad3124e4159dcc71e2b080fef38
marcerhans <marcerhans@gmail.com>
2024-06-09 10:12:09 +0200

    Minor change

--- packages/renderer/src/common.rs
+++ packages/renderer/src/common.rs
@@ -1,48 +1,46 @@
 /// Common traits and structures.
 use linear_algebra::matrix::{MatrixDataTrait, Matrix};
 
-pub trait VertexTrait {
-    type T: MatrixDataTrait;
+pub type Vertex<T> = Matrix<T>;
 
-    fn new(data: [Self::T; 3]) -> Self;
+pub trait Point<T: MatrixDataTrait> {
+    fn new(data: [T; 3]) -> Self;
 
-    fn x(&self) -> &Self::T;
-    fn y(&self) -> &Self::T;
-    fn z(&self) -> &Self::T;
+    fn x(&self) -> &T;
+    fn y(&self) -> &T;
+    fn z(&self) -> &T;
 
-    fn x_mut(&mut self) -> &mut Self::T;
-    fn y_mut(&mut self) -> &mut Self::T;
-    fn z_mut(&mut self) -> &mut Self::T;
+    fn x_mut(&mut self) -> &mut T;
+    fn y_mut(&mut self) -> &mut T;
+    fn z_mut(&mut self) -> &mut T;
 }
 
-impl<T: MatrixDataTrait> VertexTrait for Matrix<T> {
-    type T = T;
-
-    fn new(data: [Self::T; 3]) -> Self {
+impl<T: MatrixDataTrait> Point<T> for Matrix<T> {
+    fn new(data: [T; 3]) -> Self {
         Matrix::from_array([data; 1])
     }
 
-    fn x(&self) -> &Self::T {
+    fn x(&self) -> &T {
         self.index(0, 0)
     }
 
-    fn y(&self) -> &Self::T {
+    fn y(&self) -> &T {
         self.index(0, 1)
     }
 
-    fn z(&self) -> &Self::T {
+    fn z(&self) -> &T {
         self.index(0, 2)
     }
 
-    fn x_mut(&mut self) -> &mut Self::T {
+    fn x_mut(&mut self) -> &mut T {
         self.index_mut(0, 0)
     }
 
-    fn y_mut(&mut self) -> &mut Self::T {
+    fn y_mut(&mut self) -> &mut T {
         self.index_mut(0, 1)
     }
 
-    fn z_mut(&mut self) -> &mut Self::T {
+    fn z_mut(&mut self) -> &mut T {
         self.index_mut(0, 2)
     }
 }
@@ -53,7 +51,6 @@ pub trait PlaneTrait<T> {
     fn parameter_b(&self) -> &[T];
 }
 
-/// TODO: A bit ugly, because it is dependent on how matrix is implemented.
 impl<T: MatrixDataTrait> PlaneTrait<T> for Matrix<T> {
     fn point(&self) -> &[T] {
         &self.data()[0..3]
