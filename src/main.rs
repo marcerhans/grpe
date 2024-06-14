6bed72b90e494ce57e66463a60749ccd05224618
marcerhans <marcerhans@gmail.com>
2024-06-14 03:35:34 +0200

    Custom camera.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,27 @@
 use std::{thread, time::Duration};
 
 use linear_algebra::matrix::Matrix;
-use renderer::{renderer::TerminalBuilder, RendererBuilderTrait, RendererTrait};
+use renderer::{renderer::TerminalBuilder, Camera, RendererBuilderTrait, RendererTrait};
 
 fn main() {
     // 1. Create vertices
-    let mut vertices = [
-        Matrix::from_array([
-            [0.0, 0.0, 0.0],
-        ]),
-    ];
-    
+    let mut vertices = [Matrix::from_array([[0.0, 0.0, 0.0]])];
+
     // 2. Define line order
-    let line_draw_order = vec![vec![0,1], vec![0,2]];
-    
+    let line_draw_order = vec![vec![0, 1], vec![0, 2]];
+
     // 3. Render()
-    let mut renderer = TerminalBuilder::default().build();
+    // let mut renderer = TerminalBuilder::default().build();
+    let mut renderer = TerminalBuilder::default()
+        .with_camera(Camera::new(
+            (32, 32),
+            &[0.0, 0.0, 0.0],
+            &[0.0, 1.0, 0.0],
+            90,
+        ))
+        .build();
     // renderer.set_vertices_line_draw_order(&line_draw_order.iter().map(|v| v.as_slice()).collect::<Vec<&[usize]>>());
-    
+
     loop {
         thread::sleep(Duration::from_millis(500));
         renderer.set_vertices(&vertices);
