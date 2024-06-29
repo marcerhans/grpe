2d9ffc9249d8df73120c64a3a5f3e6b28075a300
marcerhans <marcerhans@gmail.com>
2024-06-29 19:25:03 +0200

    WIP terminal rendering. Have to fully implement the new method for __RendererTrait. Currently it is lacking.

--- packages/renderer/src/strategy/terminal2.rs
+++ packages/renderer/src/strategy/terminal2.rs
@@ -1,4 +1,5 @@
 use crate::{Camera, RenderOption, RendererBuilderTrait, RendererConfiguration, RendererTrait, __RendererTrait};
+use linear_algebra::vector::VectorRow;
 
 #[derive(Default)]
 struct TerminalBuilder {
@@ -28,28 +29,29 @@ impl RendererBuilderTrait for TerminalBuilder {
 }
 
 struct Terminal {
-    // config: RendererConfiguration,
+    config: RendererConfiguration,
+    vertices: Vec<VectorRow<f64, 3>>,
+    // line_draw_order: Vec<usize>, // TODO
 }
 
 impl Terminal {}
 
 impl RendererTrait for Terminal {
     fn config(&self) -> RendererConfiguration {
-        // self.config.clone()
-        RendererConfiguration::default()
+        self.config.clone()
     }
 
     fn set_config(&mut self, config: RendererConfiguration) -> Result<(), &'static str> {
-        // self.config = config;
+        self.config = config;
         Ok(())
     }
 
-    fn set_vertices(&mut self, vertices: &[linear_algebra::vector::VectorRow<f64, 3>]) {
-        todo!()
+    fn set_vertices(&mut self, vertices: &[VectorRow<f64, 3>]) {
+        self.vertices = vertices.to_owned();
     }
 
     fn set_vertices_line_draw_order(&mut self, order: &[&[usize]]) {
-        todo!()
+        todo!("Implement this later")
     }
 
     fn render(&self) {
@@ -59,7 +61,10 @@ impl RendererTrait for Terminal {
 
 impl __RendererTrait for Terminal {
     fn new(config: RendererConfiguration) -> Self {
-        Terminal { }
+        Terminal {
+            config,
+            vertices: Default::default(),
+        }
     }
 }
 
