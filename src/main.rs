6e498e6553fad64354f4166849ee42c316148321
marcerhans <marcerhans@gmail.com>
2024-07-16 00:32:18 +0200

    ...

--- src/main.rs
+++ src/main.rs
@@ -100,7 +100,7 @@ fn main() {
         frame += 1;
 
         if show_info {
-            println!("Statistics: [Frame: {frame} | Missed Frames: {frame_missed} | FPS: {fps}]");
+            println!("Frame: {frame} | Missed Frames: {frame_missed} | FPS: {fps}");
             println!(
                 "Resolution: ({},{}) | FOV: {:0>3}\nCamera Position: ({:.2},{:.2},{:.2})\nCamera Rotation: ({:.2},{:.2},{:.2},{:.2})",
                 config.camera.resolution.0, config.camera.resolution.1, config.camera.fov,
