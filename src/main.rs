aece00c30265ae051710e95c0e97d034d21aab58
marcerhans <marcerhans@gmail.com>
2024-07-15 19:33:47 +0200

    Sane FOV.

--- src/main.rs
+++ src/main.rs
@@ -71,7 +71,7 @@ fn main() {
         .with_camera(Camera {
             resolution: (128, 128),
             position: VectorRow::from([0.0, 0.0, 0.0]),
-            fov: 171,
+            fov: 90,
         })
         .build();
 
