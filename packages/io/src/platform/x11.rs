083f45a7ff0c0ae6579ea339a91ae033dfc5a3f8
marcerhans <marcerhans@gmail.com>
2024-07-17 07:21:38 +0200

    Cleaning up IO.

--- packages/io/src/platform/x11.rs
+++ packages/io/src/platform/x11.rs
@@ -1,62 +1,62 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-include!(concat!(env!("OUT_DIR"), "/unix_x11_bindings.rs"));
+// #![allow(non_upper_case_globals)]
+// #![allow(non_camel_case_types)]
+// #![allow(non_snake_case)]
+// include!(concat!(env!("OUT_DIR"), "/unix_x11_bindings.rs"));
 
-use crate::*;
+// use crate::*;
 
-pub struct IO {
-    initialized: bool,
-}
+// pub struct IO {
+//     initialized: bool,
+// }
 
-impl IO {
-    fn initialize(&mut self) {
-        self.initialized = true;
-    }
-}
+// impl IO {
+//     fn initialize(&mut self) {
+//         self.initialized = true;
+//     }
+// }
 
-impl IOTrait for IO {
-    fn read_input(&mut self) -> Key {
-        Key::KeyDown
-        // if !self.initialized {
+// impl IOTrait for IO {
+//     fn read_input(&mut self) -> Key {
+//         Key::KeyDown
+//         // if !self.initialized {
 
-        // }
-    }
-}
+//         // }
+//     }
+// }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[test]
-    fn main() {
-        unsafe {
-            let display: *mut Display = XOpenDisplay(std::ptr::null());
+//     #[test]
+//     fn main() {
+//         unsafe {
+//             let display: *mut Display = XOpenDisplay(std::ptr::null());
 
-            if !display.is_null() {
-                println!("Display!");
+//             if !display.is_null() {
+//                 println!("Display!");
 
-                let window = XCreateWindow(
-                    display,
-                    XDefaultRootWindow(display),
-                    0,
-                    0,
-                    200,
-                    100,
-                    0,
-                    CopyFromParent as i32,
-                    CopyFromParent as u32,
-                    std::ptr::null_mut(),
-                    0,
-                    std::ptr::null_mut(),
-                );
+//                 let window = XCreateWindow(
+//                     display,
+//                     XDefaultRootWindow(display),
+//                     0,
+//                     0,
+//                     200,
+//                     100,
+//                     0,
+//                     CopyFromParent as i32,
+//                     CopyFromParent as u32,
+//                     std::ptr::null_mut(),
+//                     0,
+//                     std::ptr::null_mut(),
+//                 );
 
-                XMapWindow(display, window);
-                XFlush(display);
+//                 XMapWindow(display, window);
+//                 XFlush(display);
 
-            }
-        }
+//             }
+//         }
 
-        std::thread::sleep(std::time::Duration::from_millis(10000));
-    }
-}
+//         std::thread::sleep(std::time::Duration::from_millis(10000));
+//     }
+// }
