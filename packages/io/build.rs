0e09493543c571a977e408e862eee59ab824dd0d
marcerhans <marcerhans@gmail.com>
2024-07-17 15:36:26 +0200

    Working on FFI stuff for IO.

---
+++ packages/io/build.rs
@@ -0,0 +1,35 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    // Tell cargo to look for shared libraries in the specified directory.
+    // (currently not used).
+    // println!("cargo:rustc-link-search=/usr/lib");
+
+    // Tell cargo to tell rustc to link the system [LIB]
+    // shared library.
+    // (currently not used).
+    // println!("cargo:rustc-link-lib=LIB");
+
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let ffi_src_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("src/ffi");
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header(ffi_src_path.join("unix.h").to_str().unwrap())
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("io_bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
