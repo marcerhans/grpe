383ac5e417fdc10292c65b34254f8d6665445582
marcerhans <marcerhans@gmail.com>
2024-08-22 00:01:35 +0200

    Line drawing is not evenly distributing rest/spare pixels. Do that.

--- src/model.rs
+++ src/model.rs
@@ -34,9 +34,9 @@ impl Model {
                 // }
 
                 // Exhaust
-                for around_x_axis in 0..12 {
+                for around_x_axis in 0..24 {
                     vertices.push(
-                        VectorRow::from([0.0,  0.4 * (around_x_axis as f64 * (2.0 * consts::PI / 12.0)).sin(), 0.4 * (around_x_axis as f64 * (2.0 * consts::PI / 12.0)).cos()])
+                        VectorRow::from([0.0,  0.4 * (around_x_axis as f64 * (2.0 * consts::PI / 24.0)).sin(), 0.4 * (around_x_axis as f64 * (2.0 * consts::PI / 24.0)).cos()])
                     );
                 }
 
@@ -59,20 +59,6 @@ impl Model {
                     // VectorRow::from([15.2 / 2.0, 0.0, 4.3 / 1.75]), // Wing span is 8.6 (/ 2 = 4.3)
                     // VectorRow::from([15.2 / 2.0 + 0.4, 0.0, 4.3 / 1.75]),
                     // VectorRow::from([15.2 / 2.0 + 2.0, 0.0, 1.0]),
-                ]);
-
-                // Fillers 
-                vertices.append(&mut vec![
-                    // Sidroder
-                    // VectorRow::from([0.9 + 1.0 * (0.1 / 4.0), 0.0, 1.0]),
-                    // VectorRow::from([0.9 + 2.0 * (0.1 / 4.0), 0.0, 1.5]),
-                    // VectorRow::from([0.9 + 3.0 * (0.1 / 4.0), 0.0, 2.0]),
-                    // VectorRow::from([0.9 + 4.0 * (0.1 / 4.0), 0.0, 2.5]),
-
-                    // VectorRow::from([1.7 + 1.0 * (2.5 / 5.0), 0.0, 3.0 - 1.0 * (2.5 / 5.0)]),
-                    // VectorRow::from([1.7 + 2.0 * (2.5 / 5.0), 0.0, 3.0 - 2.0 * (2.5 / 5.0)]),
-                    // VectorRow::from([1.7 + 3.0 * (2.5 / 5.0), 0.0, 3.0 - 3.0 * (2.5 / 5.0)]),
-                    // VectorRow::from([1.7 + 4.0 * (2.5 / 5.0), 0.0, 3.0 - 4.0 * (2.5 / 5.0)]),
                 ]);
 
                 for vertex in vertices.iter_mut() {
@@ -123,7 +109,8 @@ impl Model {
 
         match self {
             Model::Plane => {
-                lines.push(vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+                lines.push(vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]);
+                lines.push(vec![27, 28, 29, 30]);
             }
             Model::Spiral => {
                 let mut line = vec![];
