b741311aeec0f9467f929164a157ed26239e81ba
marcerhans <marcerhans@gmail.com>
2024-09-22 17:04:30 +0200

    Does not work :(

--- src/main.rs
+++ src/main.rs
@@ -36,7 +36,7 @@ fn main() {
     // 4. Instantiate renderer.
     let camera_default = Camera {
         resolution: args.resolution.unwrap_or((64, 64)),
-        // position: VectorRow::from([0.0, -1000.0, 0.0]),
+        position: VectorRow::from([0.0, 0.0, 0.0]),
         projection_mode: ProjectionMode::Perspective { fov: 90 },
         ..Default::default()
     };
@@ -46,32 +46,34 @@ fn main() {
         .build()
         .unwrap();
 
-    // 5. Create a state tracker
+    // 5. Create a state tracker.
     // It is a tad unclear what should/should not be part of this structure.
     // It is used mainly for tracking:
     // - Changes to input
     // - Statistics
+    // - Layout
     let mut state = State::new();
 
     // 6. Engine loop
     renderer.clear_screen();
-    while event_handler.running() {
+    // while event_handler.running() {
+    loop {
         renderer.set_vertices(Rc::clone(&vertices));
         renderer.set_vertices_line_draw_order(Rc::clone(&line_draw_order));
         renderer.render();
-        // let updated_camera = state.update(&event_handler, renderer.config().camera.clone());
-        // renderer = renderer.set_camera(updated_camera.clone()).unwrap();
+        let camera_updated = state.update(&event_handler, renderer.config().camera.clone());
+        renderer = renderer.set_camera(camera_updated.clone()).unwrap();
 
-        // let banner_text = "GRPE";
-        // let banner_fill_width = (camera.resolution.0 as usize - banner_text.len()) / 2 - 1; // Note: "-1" for extra space(s).
-        // let banner_char = "=";
-        // let banner = banner_char.repeat(banner_fill_width);
-        // print!("\x1B[H");
-        // print!("\x1B[1;38;2;0;0;0;48;2;255;255;0m{banner} {banner_text} {banner}\x1B[0m");
-        // if camera.resolution.0 % 2 != 0 {
-        //     // Just make it nice even if odd.
-        //     print!("\x1B[1;38;2;0;0;0;48;2;255;255;0m{banner_char}\x1B[0m");
-        // }
-        // println!();
+        let banner_text = "GRPE";
+        let banner_fill_width = (camera_updated.resolution.0 as usize - banner_text.len()) / 2 - 1; // Note: "-1" for extra space(s).
+        let banner_char = "=";
+        let banner = banner_char.repeat(banner_fill_width);
+        print!("\x1B[H");
+        print!("\x1B[1;38;2;0;0;0;48;2;255;255;0m{banner} {banner_text} {banner}\x1B[0m");
+        if camera_updated.resolution.0 % 2 != 0 {
+            // Just make it nice even if odd.
+            print!("\x1B[1;38;2;0;0;0;48;2;255;255;0m{banner_char}\x1B[0m");
+        }
+        println!();
     }
 }
--- src/model/test.rs
+++ src/model/test.rs
@@ -1,10 +1,17 @@
-use std::f64::consts;
-
 use renderer::VectorRow;
 
 pub fn get_vertices() -> Vec<VectorRow<f64, 3>> {
     let mut vertices = vec![];
 
+    // vertices.push(VectorRow::from([1.0, 0.0, 0.0]));
+    // vertices.push(VectorRow::from([-1.0, 0.0, 0.0]));
+    // vertices.push(VectorRow::from([1.0, 0.0, 0.0]));
+    // vertices.push(VectorRow::from([0.0, 0.0, 1.0]));
+    // vertices.push(VectorRow::from([0.0, 0.0, -1.0]));
+
+    for vertex in vertices.iter_mut() {
+        // vertex.0.scale(1.0);
+    }
 
     vertices
 }
@@ -12,6 +19,10 @@ pub fn get_vertices() -> Vec<VectorRow<f64, 3>> {
 pub fn get_line_draw_order() -> Vec<Vec<usize>> {
     let mut lines = vec![];
 
+    // lines.push(vec![0,1]);
+    // lines.push(vec![0,2]);
+    // lines.push(vec![0,3]);
+    // lines.push(vec![0,4]);
 
     lines
 }
