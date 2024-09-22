f7e60eb43836d1d04bd6fbb0e1adc0156525c9de
marcerhans <marcerhans@gmail.com>
2024-09-22 17:10:41 +0200

    ...

--- src/model/test.rs
+++ src/model/test.rs
@@ -3,11 +3,11 @@ use renderer::VectorRow;
 pub fn get_vertices() -> Vec<VectorRow<f64, 3>> {
     let mut vertices = vec![];
 
-    // vertices.push(VectorRow::from([1.0, 0.0, 0.0]));
-    // vertices.push(VectorRow::from([-1.0, 0.0, 0.0]));
-    // vertices.push(VectorRow::from([1.0, 0.0, 0.0]));
-    // vertices.push(VectorRow::from([0.0, 0.0, 1.0]));
-    // vertices.push(VectorRow::from([0.0, 0.0, -1.0]));
+    vertices.push(VectorRow::from([0.0, 0.0, 0.0]));
+    vertices.push(VectorRow::from([-1.0, 0.0, 0.0]));
+    vertices.push(VectorRow::from([1.0, 0.0, 0.0]));
+    vertices.push(VectorRow::from([0.0, 0.0, 1.0]));
+    vertices.push(VectorRow::from([0.0, 0.0, -1.0]));
 
     for vertex in vertices.iter_mut() {
         // vertex.0.scale(1.0);
@@ -19,10 +19,10 @@ pub fn get_vertices() -> Vec<VectorRow<f64, 3>> {
 pub fn get_line_draw_order() -> Vec<Vec<usize>> {
     let mut lines = vec![];
 
-    // lines.push(vec![0,1]);
-    // lines.push(vec![0,2]);
-    // lines.push(vec![0,3]);
-    // lines.push(vec![0,4]);
+    lines.push(vec![0,1]);
+    lines.push(vec![1,4]);
+    lines.push(vec![4,2]);
+    lines.push(vec![2,3]);
 
     lines
 }
