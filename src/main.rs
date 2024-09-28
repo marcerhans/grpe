199852360adba02eddf0854aee9eddd056cb4d4c
marcerhans <marcerhans@gmail.com>
2024-09-28 22:15:27 +0200

    Ready to model plane.

--- src/main.rs
+++ src/main.rs
@@ -35,8 +35,8 @@ fn main() {
         .unwrap();
 
     let mut extras = renderer.extras().clone();
-    extras.pixel_width_scaling = 47.5 / 31.5; // Just for drafting model from image. Shoule be 1.0.
-    extras.pixel_height_scaling = 1.0; // Just for drafting model from image. Shoule be 1.0.
+    extras.pixel_width_scaling = 200.0 * (47.5 / 31.5); // Just for drafting model from image. Shoule be 1.0.
+    extras.pixel_height_scaling = 200.0; // Just for drafting model from image. Shoule be 1.0.
     renderer.set_extras(extras);
 
     // 3. Create vertices.
