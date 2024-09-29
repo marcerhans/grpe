debd8e31bb20f3c13d57e1acda8d9178113176db
marcerhans <marcerhans@gmail.com>
2024-09-29 10:57:31 +0200

    Canards positions and lines added. Thickness wip.

--- src/model/plane.rs
+++ src/model/plane.rs
@@ -219,32 +219,32 @@ mod wings {
     pub fn get_vertices() -> Vec<VectorRow<f64, 3>> {
         let mut vertices = Vec::new();
 
-        // Outline
         vertices.append(&mut vec![
-            VectorRow::from([2.68, 1.0, 0.0]), // --
+            // Outline
+            VectorRow::from([2.68, 1.0, 0.0]), // +
             VectorRow::from([3.07, 1.45, 0.0]),
-            VectorRow::from([3.15, 2.2, 0.0]), // --
-            VectorRow::from([3.19, 3.17, 0.0]), // --
+            VectorRow::from([3.15, 2.2, 0.0]),  // +
+            VectorRow::from([3.19, 3.17, 0.0]), // +
             VectorRow::from([3.24, 4.3, 0.0]),
-            VectorRow::from([4.3, 4.3, 0.0]), // |
+            VectorRow::from([4.3, 4.3, 0.0]), // +
             VectorRow::from([5.95, 3.17, 0.0]),
-            VectorRow::from([5.65, 3.17, 0.0]), // |
-            VectorRow::from([6.85, 2.2, 0.0]), // --
+            VectorRow::from([5.65, 3.17, 0.0]), // +
+            VectorRow::from([6.85, 2.2, 0.0]),  // +
             VectorRow::from([8.15, 1.26, 0.0]),
             VectorRow::from([8.8, 0.98, 0.0]),
-
-            VectorRow::from([4.3, 3.17, 0.1]), // +
-            VectorRow::from([4.3, 2.2, 0.1]), // +
+            // +Z
+            VectorRow::from([4.3, 3.17, 0.1]),  // +
+            VectorRow::from([4.3, 2.2, 0.1]),   // +
             VectorRow::from([4.3, 1.0, 0.125]), // +
-            VectorRow::from([5.65, 2.2, 0.1]), // +
-            VectorRow::from([5.65, 1.0, 0.1]), // +
+            VectorRow::from([5.65, 2.2, 0.1]),  // +
+            VectorRow::from([5.65, 1.0, 0.1]),  // +
             VectorRow::from([6.85, 1.0, 0.09]), // +
-
-            VectorRow::from([4.3, 3.17, -0.1]), // +
-            VectorRow::from([4.3, 2.2, -0.1]), // +
+            // -Z
+            VectorRow::from([4.3, 3.17, -0.1]),  // +
+            VectorRow::from([4.3, 2.2, -0.1]),   // +
             VectorRow::from([4.3, 1.0, -0.125]), // +
-            VectorRow::from([5.65, 2.2, -0.1]), // +
-            VectorRow::from([5.65, 1.0, -0.1]), // +
+            VectorRow::from([5.65, 2.2, -0.1]),  // +
+            VectorRow::from([5.65, 1.0, -0.1]),  // +
             VectorRow::from([6.85, 1.0, -0.09]), // +
         ]);
 
@@ -282,73 +282,19 @@ mod wings {
                     start + next(&mut count),
                 ],
                 // +Z
-                vec![
-                    start + 0,
-                    start + 13,
-                    start + 15,
-                    start + 16,
-                    start + 10,
-                ],
-                vec![
-                    start + 2,
-                    start + 12,
-                    start + 14,
-                    start + 8,
-                ],
-                vec![
-                    start + 3,
-                    start + 11,
-                    start + 7,
-                ],
-                vec![
-                    start + 5,
-                    start + 11,
-                    start + 12,
-                    start + 13,
-                ],
-                vec![
-                    start + 7,
-                    start + 14,
-                    start + 15,
-                ],
-                vec![
-                    start + 8,
-                    start + 16,
-                ],
+                vec![start + 0, start + 13, start + 15, start + 16, start + 10],
+                vec![start + 2, start + 12, start + 14, start + 8],
+                vec![start + 3, start + 11, start + 7],
+                vec![start + 5, start + 11, start + 12, start + 13],
+                vec![start + 7, start + 14, start + 15],
+                vec![start + 8, start + 16],
                 // -Z
-                vec![
-                    start + 0,
-                    start + 19,
-                    start + 21,
-                    start + 22,
-                    start + 10,
-                ],
-                vec![
-                    start + 2,
-                    start + 18,
-                    start + 20,
-                    start + 8,
-                ],
-                vec![
-                    start + 3,
-                    start + 17,
-                    start + 7,
-                ],
-                vec![
-                    start + 5,
-                    start + 17,
-                    start + 18,
-                    start + 19,
-                ],
-                vec![
-                    start + 7,
-                    start + 20,
-                    start + 21,
-                ],
-                vec![
-                    start + 8,
-                    start + 22,
-                ],
+                vec![start + 0, start + 19, start + 21, start + 22, start + 10],
+                vec![start + 2, start + 18, start + 20, start + 8],
+                vec![start + 3, start + 17, start + 7],
+                vec![start + 5, start + 17, start + 18, start + 19],
+                vec![start + 7, start + 20, start + 21],
+                vec![start + 8, start + 22],
             ]);
 
             ldo
@@ -374,12 +320,23 @@ mod canards {
         vertices.append(&mut vec![
             VectorRow::from([8.68, 0.98, 0.0]),
             VectorRow::from([8.54, 1.08, 0.0]),
-            VectorRow::from([8.54, 1.28, 0.0]),
+            VectorRow::from([8.54, 1.28, 0.0]), // +
+            VectorRow::from([8.25, 1.76, 0.0]), // +
             VectorRow::from([7.93, 2.32, 0.0]),
-            VectorRow::from([8.42, 2.32, 0.0]),
+            VectorRow::from([8.42, 2.32, 0.0]),  // +
+            VectorRow::from([9.33, 1.76, 0.0]),  // +
+            VectorRow::from([10.12, 1.28, 0.0]), // +
             VectorRow::from([10.43, 1.09, 0.0]),
-            VectorRow::from([10.2, 1.04, 0.0]),
-            VectorRow::from([9.5, 1.0, 0.0]),
+            // +Z
+            VectorRow::from([8.54, 1.76, 0.1]), // +
+            VectorRow::from([9.33, 1.28, 0.1]), // +
+            VectorRow::from([9.33, 1.0, 0.1]),  // +
+            VectorRow::from([10.12, 1.02, 0.1]), // +
+            // -Z
+            VectorRow::from([8.54, 1.76, -0.1]), // +
+            VectorRow::from([9.33, 1.28, -0.1]), // +
+            VectorRow::from([9.33, 1.0, -0.1]),  // +
+            VectorRow::from([10.12, 1.02, -0.1]), // +
         ]);
 
         // Duplicate and mirror.
@@ -391,32 +348,52 @@ mod canards {
     pub fn get_line_draw_order(start: usize) -> Vec<Vec<usize>> {
         let mut line_draw_order = vec![];
 
+        let next = |x: &mut usize| {
+            let prev = *x;
+            *x += 1;
+            prev
+        };
+
+        let add_lines = |start: usize, mut ldo: Vec<Vec<usize>>| -> Vec<Vec<usize>> {
+            let mut count = 0;
+
+            ldo.append(&mut vec![
+                // Outline
+                vec![
+                    start + next(&mut count),
+                    start + next(&mut count),
+                    start + next(&mut count),
+                    start + next(&mut count),
+                    start + next(&mut count),
+                    start + next(&mut count),
+                    start + next(&mut count),
+                    start + next(&mut count),
+                    start + next(&mut count),
+                ],
+                // +Z
+                vec![start + 0, start + 11, start + 12, start + 8],
+                vec![start + 2, start + 10, start + 7],
+                vec![start + 3, start + 9, start + 6],
+                vec![start + 5, start + 9, start + 2],
+                vec![start + 6, start + 11],
+                vec![start + 7, start + 12],
+                // -Z
+                vec![start + 0, start + 15, start + 16, start + 8],
+                vec![start + 2, start + 14, start + 7],
+                vec![start + 3, start + 13, start + 6],
+                vec![start + 5, start + 13, start + 2],
+                vec![start + 6, start + 15],
+                vec![start + 7, start + 16],
+            ]);
+
+            ldo
+        };
+
         // Outline
-        line_draw_order.append(&mut vec![vec![
-            start + 0,
-            start + 1,
-            start + 2,
-            start + 3,
-            start + 4,
-            start + 5,
-            start + 6,
-            start + 7,
-            start + 0,
-        ]]);
+        line_draw_order = add_lines(start, line_draw_order);
 
         // Mirror
-        let start = start + 8;
-        line_draw_order.append(&mut vec![vec![
-            start + 0,
-            start + 1,
-            start + 2,
-            start + 3,
-            start + 4,
-            start + 5,
-            start + 6,
-            start + 7,
-            start + 0,
-        ]]);
+        line_draw_order = add_lines(start + 17, line_draw_order);
 
         line_draw_order
     }
@@ -467,7 +444,7 @@ pub fn get_vertices() -> Vec<VectorRow<f64, 3>> {
     // Scale and center
     for vertex in vertices.iter_mut() {
         vertex[0] = vertex[0] - 15.7 / 2.0; // Center plane
-        // vertex.0.scale(20.0);
+                                            // vertex.0.scale(20.0);
     }
 
     vertices
@@ -498,9 +475,9 @@ pub fn get_line_draw_order() -> Vec<Vec<usize>> {
     index_start += wings::get_vertices().len();
     line_draw_order.append(&mut wings);
 
-    // let mut canards = canards::get_line_draw_order(index_start);
-    // index_start += canards::get_vertices().len();
-    // line_draw_order.append(&mut canards);
+    let mut canards = canards::get_line_draw_order(index_start);
+    index_start += canards::get_vertices().len();
+    line_draw_order.append(&mut canards);
 
     // let mut intake = intake::get_line_draw_order(index_start);
     // index_start += intake::get_vertices().len();
