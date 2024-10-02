0f983d735110eaa145f635a9d4e161ff526dc84e
marcerhans <marcerhans@gmail.com>
2024-10-02 18:37:57 +0200

    Only resize if resolution has not been set.

--- src/state.rs
+++ src/state.rs
@@ -349,9 +349,11 @@ impl StateHandler {
         }
 
         // Resize
-        if let Some(new_size) = self.input.misc.resize {
-            config.camera.resolution.0 = new_size.0;
-            config.camera.resolution.1 = new_size.1 * 2 - 4; // "* 2 - 4" because we want to make space for title and info.
+        if let None = self.args.resolution {
+            if let Some(new_size) = self.input.misc.resize {
+                config.camera.resolution.0 = new_size.0;
+                config.camera.resolution.1 = new_size.1 * 2 - 4; // "* 2 - 4" because we want to make space for title and info.
+            }
         }
 
         // Calculate rotational change based on input.
